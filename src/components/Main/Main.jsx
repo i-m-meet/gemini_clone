14aafaf83ec53f705da4b44c848ee9352a5afc71
Sumeet Sumeet <sumeet.bx1@gmail.com>
2025-02-07 22:25:18 -0500

    main body UI done

---
+++ src/components/Main/Main.jsx
@@ -0,0 +1,87 @@
+import { assets } from "../../assets/assets";
+
+const Main = () => {
+  return (
+    <div className="flex-1 min-h-screen pb-4 relative">
+      <div className="flex items-center justify-between text-lg p-5 text-[#585858]">
+        <p>Gemini</p>
+        <img
+          src={assets.user_icon}
+          alt="user icon"
+          className="w-10 h-10 rounded-full"
+        />
+      </div>
+
+      <div className="max-w-225 m-auto">
+        <div className="mt-13 mb-13 text-6xl color-[#c4c7c5] text-gray-300 p-5">
+          <p>
+            <span className="custom-gradient">Hello, User</span>
+          </p>
+          <p className="font-semibold mt-4">How can I help you today!</p>
+        </div>
+        <div className="custom-grid-cards gap-4 p-5">
+          <div className="h-50 p-4 bg-[#f0f4f9] rounded-2xl relative cursor-pointer hover:bg-[#dfe4ea]">
+            <p className="text-[#585858] ">
+              Suggest beautiful places to see on an upcoming road trip{" "}
+            </p>
+            <img
+              src={assets.compass_icon}
+              alt=""
+              className="w-9 p-1 absolute bg-white rounded-2xl bottom-2 right-2"
+            />
+          </div>
+          <div className="h-50 p-4 bg-[#f0f4f9] rounded-2xl relative cursor-pointer hover:bg-[#dfe4ea]">
+            <p className="text-[#585858] ">
+              Briefly summarize this concept urban planning{" "}
+            </p>
+            <img
+              src={assets.bulb_icon}
+              alt=""
+              className="w-9 p-1 absolute bg-white rounded-2xl bottom-2 right-2"
+            />
+          </div>
+          <div className="h-50 p-4 bg-[#f0f4f9] rounded-2xl relative cursor-pointer hover:bg-[#dfe4ea]">
+            <p className="text-[#585858] ">
+              Brainstorm team bonding activities for our woek retreat{" "}
+            </p>
+            <img
+              src={assets.message_icon}
+              alt=""
+              className="w-9 p-1 absolute bg-white rounded-2xl bottom-2 right-2"
+            />
+          </div>
+          <div className="h-50 p-4 bg-[#f0f4f9] rounded-2xl relative cursor-pointer hover:bg-[#dfe4ea]">
+            <p className="text-[#585858] ">
+              Improve the readability of the following code
+            </p>
+            <img
+              src={assets.code_icon}
+              alt=""
+              className="w-9 p-1 absolute bg-white rounded-2xl bottom-2 right-2"
+            />
+          </div>
+        </div>
+        <div className="absolute bottom-0 w-full max-w-225 py-0 px-5 m-auto ">
+          <div className="flex items-center justify-between gap-5 bg-[#f0f4f9] p-3 rounded-4xl">
+            <input
+              type="text"
+              placeholder="Enter a prompt here"
+              className=" flex-1 w-180 h-12 outline-none ml-3 text-gray-700"
+            />
+            <div className="flex cursor-pointer gap-2">
+              <img src={assets.gallery_icon} alt="" className="w-6 h-6" />
+              <img src={assets.mic_icon} alt="" className="w-6 h-6" />
+              <img src={assets.send_icon} alt="" className="w-6 h-6" />
+            </div>
+          </div>
+          <p className="text-sm mt-4 mb-4 ml-13">
+            Gemini may display inaccurate info, including about people, so
+            double-check its responses. Your privacy and Gemini Apps{" "}
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Main;
--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,11 +1,15 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import { assets } from "../../assets/assets";
 
 const Sidebar = () => {
   const [isOpen, setIsOpen] = useState(false);
 
   return (
-    <div className={`min-h-screen ${isOpen? "w-60" : "w-30"} bg-[#f0f4f9] p-6 flex flex-col justify-between shadow-lg`}>
+    <div
+      className={`min-h-screen ${
+        isOpen ? "w-60" : "w-30"
+      } bg-[#f0f4f9] p-6 flex flex-col justify-between shadow-lg font-outline`}
+    >
       {/* Top Section (Menu Icon and New Chat) */}
       <div className="flex flex-col gap-8">
         {/* Menu Icon */}
@@ -13,27 +17,31 @@ const Sidebar = () => {
           src={assets.menu_icon}
           alt="Menu Icon"
           className="w-8 h-8 cursor-pointer"
-          onClick={() => setIsOpen(prev => !prev) }
+          onClick={() => setIsOpen((prev) => !prev)}
         />
 
         {/* New Chat Button */}
         <div className="mt-10 w-full flex items-center justify-center gap-4 py-4 px-4 bg-[#e6eaf1] rounded-3xl text-gray-900 text-md hover:bg-[#d0d8e2] cursor-pointer transition-all duration-300">
           <img src={assets.plus_icon} alt="Plus Icon" className="w-6 h-8" />
-          {isOpen? <p className="font-extralight text-gray-500 hover:text-gray-900 hover:font-semibold">
-            New Chat
-          </p> : null}
+          {isOpen ? (
+            <p className="font-extralight text-gray-500 hover:text-gray-900 hover:font-semibold">
+              New Chat
+            </p>
+          ) : null}
         </div>
 
         {/* Recent Section */}
         <div className="flex flex-col gap-2">
-          {isOpen? <p className="font-semibold text-gray-700">Recent</p>: null}
+          {isOpen ? (
+            <p className="font-semibold text-gray-700">Recent</p>
+          ) : null}
           <div className="flex items-center gap-2 p-3 pr-3 rounded-3xl text-[#282828] hover:bg-[#e2e6eb] cursor-pointer transition-all duration-300">
             <img
               src={assets.message_icon}
               alt="Message Icon"
               className="w-8 h-8"
             />
-            {isOpen? <p>What is react...</p>: null}
+            {isOpen ? <p>What is react...</p> : null}
           </div>
         </div>
       </div>
@@ -42,7 +50,7 @@ const Sidebar = () => {
       <div className="flex flex-col gap-4">
         <div className="flex items-center gap-4 text-xl hover:bg-[#e2e6eb] cursor-pointer rounded-3xl p-3 transition-all duration-300">
           <img src={assets.question_icon} alt="Help Icon" className="w-8 h-8" />
-         {isOpen?<p>Help</p>: null}
+          {isOpen ? <p>Help</p> : null}
         </div>
         <div className="flex items-center gap-4 text-xl hover:bg-[#e2e6eb] cursor-pointer rounded-3xl p-3 transition-all duration-300">
           <img
@@ -50,7 +58,7 @@ const Sidebar = () => {
             alt="History Icon"
             className="w-8 h-8"
           />
-         {isOpen?<p>History</p>: null}
+          {isOpen ? <p>History</p> : null}
         </div>
         <div className="flex items-center gap-4 text-xl hover:bg-[#e2e6eb] cursor-pointer rounded-3xl p-3 transition-all duration-300">
           <img
@@ -58,7 +66,7 @@ const Sidebar = () => {
             alt="Settings Icon"
             className="w-8 h-8"
           />
-         {isOpen?<p>Settings</p>: null}
+          {isOpen ? <p>Settings</p> : null}
         </div>
       </div>
     </div>
